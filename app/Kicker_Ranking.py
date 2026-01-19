35291ad356873f7a93067507f1c92306d280d0c2
Nico Oerter <nico.oerter@gmx.de>
2026-01-19 20:53:09 +0100

    implented sorting by wins and then goals

--- app/Kicker_Ranking.py
+++ app/Kicker_Ranking.py
@@ -58,7 +58,7 @@ if st.button("Match speichern"):
 st.header("Leaderboard")
 leaderboard_df = get_leaderboard_df()
 df_sorted = (
-    leaderboard_df.sort_values("Siege", ascending=False).reset_index(drop=True)
+    leaderboard_df.sort_values(by=["Siege", "Tore"], ascending=[False, False]).reset_index(drop=True)
 )
 df_sorted.index = df_sorted.index + 1
 df_sorted.index.name = "Platz"
